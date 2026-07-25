f9a05e5877ae06121881c83018c979f97dbf2851
irfanrumman <mdirfansayid@gmail.com>
2026-07-25 23:40:38 +0600

    project structure is ready

---
+++ app/(public)/news/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewsByIdPage = () => {
+  return (
+    <div>News By Id Page</div>
+  )
+}
+
+export default NewsByIdPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
       lang="en"
       className="h-full antialiased"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+        {/* navbar */}
+
+        {children}
+        
+        {/* footer */}
+      
+        </body>
     </html>
   );
 }
